5c2bccb2c87c210a8304fdc2f28637526f42b5a6
Nik Karavias <karavias.nikos@gmail.com>
2024-01-28 10:10:54 -0500

    remove actions-deprecated

--- asset_inventory_checks/actions-deprecated/base_action.py
+++
@@ -1,6 +0,0 @@
-class Action:
-    def __init__(self, action_type):
-        self.type = action_type
-
-    def execute(self):
-        raise NotImplementedError("Must override execute in subclass")
